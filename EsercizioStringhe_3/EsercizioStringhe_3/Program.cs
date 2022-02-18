97eba6e8463ca6c9f025ba38c9d449395ee5dcb5
rebb <98151576+rebbbbb@users.noreply.github.com>
2022-02-18 09:41:43 +0100

    EserciziStringhe

---
+++ EsercizioStringhe_3/EsercizioStringhe_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EsercizioStringhe_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scrivi un programma che, inserita una sequenza di parole, calcoli quante, fra le parole
+            //inserite, iniziano e terminano con la stessa lettera;
+
+            string parola = "";
+            int InizioFineUguale = 0;
+
+            while (parola != "fine")
+            {
+                Console.WriteLine("Inserisci una parola: ");
+                parola = Console.ReadLine();
+
+                int lungh = parola.Length;
+                InizioFineUguale = Convert.ToInt32(Console.ReadLine());
+
+                if (lungh == InizioFineUguale)
+                {
+                    InizioFineUguale++;
+                }
+            }
+            Console.WriteLine($"Le parole che iniziano e finiscono con la stessa lettera sono: {InizioFineUguale}");
+        }
+    }
+}
---
+++ EsercizioStringhe_9/EsercizioStringhe_9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EsercizioStringhe_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scrivi un programma che, inserita una sequenza di parole, calcoli quante, fra le parole
+            //inserite, hanno un numero pari di lettere e quante un numero dispari;
+
+            string parola = "";
+            int dispari = 0;
+            int pari = 0;
+
+            while (parola != "fine");
+            {
+                Console.WriteLine("Inserisci una parola: ");
+                parola = Console.ReadLine();
+
+                int lungh = parola.Length;
+
+                if ((lungh % 2) != 0)
+                {
+                    parola = parola + pari;
+                }
+                else
+                {
+                    parola = parola + dispari;
+                }
+            }
+            Console.WriteLine($"Le parole che hanno un numero pari di lettere sono: {pari}");
+            Console.WriteLine($"Le parole che hanno un numero dispari di lettere sono: {dispari}");
+        }
+    }
+}
